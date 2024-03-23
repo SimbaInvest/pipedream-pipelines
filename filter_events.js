c70d56cff21e899a19ad0b557924f9b98d96886e
Simba Invest <simbainvest.pro@gmail.com>
2024-03-23 21:34:08 +0100

    get tomorrow events and exit flow is events array is empty

--- filter_events.js
+++ filter_events.js
@@ -1,18 +1,22 @@
 // To use previous step data, pass the `steps` object to the run() function
 export default defineComponent({
     async run({ steps, $ }) {
-        var sources = [];
-        for(const event of steps.coinmarketcal_search_events.$return_value) {
-            var dateToday = new Date().toDateString();
-            var dateEvent = new Date(event.date_event).toDateString();
-            if(dateToday >= dateEvent && dateToday <= dateEvent){
-                sources.push("- " + event.source);
-            }
+      var sources = [];
+      for(const event of steps.coinmarketcal_search_events.$return_value) {
+        var dateTomorrow = new Date();
+        dateTomorrow.setDate(dateTomorrow.getDate() + 1)
+        dateTomorrow = dateTomorrow.toDateString()
+        var dateEvent = new Date(event.date_event).toDateString();
+        console.log(dateTomorrow);
+        console.log(dateEvent)
+        if(dateTomorrow >= dateEvent && dateTomorrow <= dateEvent){
+          sources.push("- " + event.source);
         }
-        if(sources.length != 0){
-            $.export('sources', "Evénements prévus aujourd'hui :\n" + sources.join("\n"));
-        } else {
-            $.export('sources', "Evénements prévus aujourd'hui : Aucun événement prévu")
-        }
+      }
+      if(sources.length != 0){
+        $.export('sources', "Evénements prévus demain :\n" + sources.join("\n"));
+      } else {
+        return $.flow.exit();
+      }
     },
-})
+  })
